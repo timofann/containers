bb983f8590ee5b83643627575f75438df7db21e1
dedelmir <anytat@inbox.ru>
2022-09-16 01:57:14 +0300

    draft draft: add iterator and inputIterator

--- iterators.hpp
+++ iterators.hpp
@@ -1,30 +1,117 @@
 #ifndef ITERATORS_HPP
 #define ITERATORS_HPP
 
+#include <iostream>
+#include <type_traits>
+
 namespace ft {
 
-	template <typename container>
-	class InputIterator {
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag : public input_iterator_tag {};
+	struct bidirectional_iterator_tag : public forward_iterator_tag {};
+	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+
+	template<bool B, typename T1, typename T2>
+	struct conditional { typedef T2 type; };
+	template<typename T1, typename T2>
+	struct conditional<false, T1, T2> { typedef T1 type; };
+
+
+	template <bool IsConst, typename T, typename Category>
+	class Iterator {
 	public:
-		 friend container;
-	};
+		typedef T value_type;
+		typedef T& reference;
+		typedef std::ptrdiff_t difference_type;
+		typedef T* pointer;
+		typedef Category iterator_category;
 
+	protected:
+		pointer _ptr;
+		typedef const T& const_reference;
 
-	class iterator {
-	private:
-		value_type *value_ptr_;
+//		template<bool B, typename T1, typename T2>
+//		struct conditional { typedef T2 type; };
+//		template<typename T1, typename T2>
+//		struct conditional<false, T1, T2> { typedef T1 type; };
+
 	public:
-		reference operator*() { return *value_ptr_; };
-	};
+		explicit Iterator() : _ptr(0) {};
+		explicit Iterator(value_type* ptr) : _ptr(ptr) {};
+		Iterator(const Iterator<true, T, Category>& copy) { _ptr = copy._ptr; };
+		Iterator(const Iterator<false, T, Category>& copy) { _ptr = copy._ptr; };
+		Iterator &operator=(const Iterator<true, T, Category>& copy) {
+			_ptr = copy._ptr;
+			return *this;
+		};
+		Iterator&
+		operator=(const Iterator<false, T, Category>& copy) {
+			_ptr = copy._ptr;
+			return *this;
+		};
+		typename ft::conditional<IsConst, Iterator, const Iterator>::type&
+		operator++() {
+			++_ptr;
+			return *this;
+		};
+		typename ft::conditional<IsConst, reference, const_reference>::type
+		operator*() const { return *_ptr; };
+		~Iterator() {};
 
-	class const_iterator {
-	private:
-		value_type *value_ptr_;
+	}; // class Iterator
+
+
+	template <bool IsConst, typename T>
+	class InputIterator : public Iterator< IsConst, T, input_iterator_tag> {
 	public:
-		const_reference operator*() { return *value_ptr_; };
-	};
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::value_type value_type;
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::reference reference;
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::pointer pointer;
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::difference_type difference_type;
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::iterator_category iterator_category;
+
+	protected:
+		typedef typename Iterator< IsConst, T, input_iterator_tag>::const_reference const_reference;
+
+	public:
+		explicit InputIterator() : Iterator<IsConst, T, input_iterator_tag>() {};
+		explicit InputIterator(value_type* ptr) : Iterator<IsConst, T, input_iterator_tag>(ptr) {};
+
+		InputIterator &operator=(const InputIterator& copy) {
+			Iterator<IsConst, T, input_iterator_tag>::operator=(copy);
+			return *this;
+		};
+
+		typename ft::conditional<IsConst, reference, const_reference>::type
+		operator*() const;
+
+		typename ft::conditional<IsConst, InputIterator, const InputIterator>::type&
+		operator++() {
+			++Iterator<IsConst, T, input_iterator_tag>::_ptr;
+			return *this;
+		};
+
+		const InputIterator operator++(int) {
+			InputIterator tmp = *this;
+			++Iterator<IsConst, T, input_iterator_tag>::_ptr;
+			return tmp;
+		};
+
+		bool operator==(const InputIterator& other) const {
+			return Iterator<IsConst, T, input_iterator_tag>::_ptr == other.Iterator<IsConst, T, input_iterator_tag>::_ptr;
+		};
+
+		bool operator!=(const InputIterator& other) const {
+			return Iterator<IsConst, T, input_iterator_tag>::_ptr != other.Iterator<IsConst, T, input_iterator_tag>::_ptr;
+		};
+
+//		using Iterator<IsConst, T, input_iterator_tag>::operator++;
+
+	}; // class InputIterator
 
 
-}
+} // namespace ft
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
 #include <vector>
+//#include "test.hpp"
+#include "iterators.hpp"
+#include "vector.hpp"
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
-	std::vector<int> vec(0);
-	std::cout << *(vec.begin()) << std::endl;
+	ft::vector<std::string> x(6, "anna");
+	x[1] = "toliy";
+	x[2] = "anya";
+	x[3] = "anytochka";
+	x[4] = "bobah";
+	x[5] = "bobka";
+	ft::vector<std::string>::iterator it(&x[3]);
+	*it = x[3];
+//
+	++it;
+//
+	std::cout << *it << std::endl;
+
+//	int arr[6];
+//	arr[0] = 1;
+//	ft::InputIterator<false, int> it;
+//	it = arr;
+//	*it = 2;
+//	std::cout << arr[0] << std::endl;
+//	std::vector<int> vec(0);
+//	std::cout << *(vec.begin()) << std::endl;
+//	ft::pair<std::string, std::string> as("dgsh", "gf");
+//	ft::pair<std::string, std::string> n(as);
+//
+//	int x = 1;
+//	int y = 0;
+//	std::cout << (x == y) << std::endl;
+//	int vec[6];
+//	vec[0] = 0;
+//	vec[1] = 1;
+//	vec[2] = 2;
+//	vec[3] = 3;
+//	vec[4] = 4;
+//	vec[5] = 5;
+//	int *p1 = vec;
+//	int *p2 = &vec[5];
+//
+//	std::cout << p1 << " " << p2 << " " << p1 - p2 << std::endl;
+
+//	std::vector<int> vec(6);
+//	vec[0] = 0;
+//	vec[1] = 1;
+//	vec[2] = 2;
+//	vec[3] = 3;
+//	vec[4] = 4;
+//	vec[5] = 5;
+//	std::iterator_traits<std::input_iterator_tag> it1 = vec.end();
+//	std::iterator_traits<std::input_iterator_tag> it2 = vec.begin();
+//	std::cout << it1 << std::endl;
+
+
+
+
+
+
+
+
 	return 0;
 }
