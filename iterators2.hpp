f0695110b13b53845dbfaa4f81a75c8c692a5c55
dedelmir <anytat@inbox.ru>
2022-09-28 01:51:17 +0300

    advance, draft iterator_traits, type_traits_file for metafunctions

--- iterators2.hpp
+++ iterators2.hpp
@@ -2,48 +2,69 @@
 #define CONTAINERS_ITERATORS2_HPP
 
 #include <iostream>
+#include "type_traits.hpp"
 
 namespace ft {
 
-
-	template<bool IsConst, typename non_const_T, typename const_T>
-	struct conditional { typedef non_const_T type; };
-	template<typename non_const_T, typename const_T>
-	struct conditional<true, non_const_T, const_T> { typedef const_T type; };
-
-
-	template<typename T>
-	struct remove_const { typedef T type; };
-	template<typename T>
-	struct remove_const<const T> { typedef T type; };
-
-
-	template<bool condition, typename T = void>
-	struct enable_if {};
-	template<typename T>
-	struct enable_if<true, T> { typedef T type; };
-
-
 	struct input_iterator_tag {};
-	struct forward_iterator_tag : public input_iterator_tag {};
-	struct bidirectional_iterator_tag : public forward_iterator_tag {};
-	struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+	struct forward_iterator_tag         :   public input_iterator_tag {};
+	struct bidirectional_iterator_tag   :   public forward_iterator_tag {};
+	struct random_access_iterator_tag   :   public bidirectional_iterator_tag {};
+
+
+	template <typename It> //почитать еще про traits и посмотреть реализации
+	struct iterator_traits {
+		typedef typename It::value_type         value_type;
+		typedef typename It::reference          reference;
+		typedef typename It::difference_type    difference_type;
+		typedef typename It::pointer            pointer;
+		typedef typename It::iterator_category  iterator_category;
+	};
+
+
+	namespace detail {
+
+		template <typename It>
+		void do_advance(It& it, typename iterator_traits<It>::difference_type n, input_iterator_tag) {
+			while (n > 0) { --n; ++it; }
+		}
+
+		template <typename It>
+		void do_advance(It& it, typename iterator_traits<It>::difference_type n, bidirectional_iterator_tag) {
+			while (n > 0) { --n; ++it; }
+			while (n < 0) { ++n; --it; }
+		}
+
+		template<class It>
+		void do_advance(It& it, typename iterator_traits<It>::difference_type n, std::random_access_iterator_tag) {
+			it += n;
+		}
+
+	} // namespace detail
+
+
+	template <typename InputIt, typename Distance>
+	void advance(InputIt& it, Distance n) {
+		detail::do_advance(it, typename iterator_traits<InputIt>::difference_type(n), typename iterator_traits<InputIt>::iterator_category());
+	}
 
 
 	template <bool IsConst, typename T, typename Category>
 	class Iterator {
 	public:
-		typedef T value_type;
-		typedef T& reference;
-		typedef long difference_type;
-		typedef T* pointer;
-		typedef Category iterator_category;
+		typedef T           value_type;
+		typedef T&          reference;
+		typedef long        difference_type;
+		typedef T*          pointer;
+		typedef Category    iterator_category;
 	protected:
 		T* _ptr;
 	public:
 		explicit Iterator() : _ptr(nullptr) {};
 		explicit Iterator(T* ptr) : _ptr(ptr) {};
-		Iterator<IsConst, T, Category>(const Iterator<IsConst, T, Category>& copy) : _ptr(copy.base()) {};
+//		Iterator<IsConst, T, Category>(const Iterator<IsConst, T, Category>& copy) : _ptr(copy.base()) {};
+		Iterator(const Iterator<false, T, Category>& copy) : _ptr(copy.base()) {};
+		Iterator(const Iterator<true, T, Category>& copy) : _ptr(copy.base()) {};
 		Iterator&
 		operator=(const Iterator<IsConst, T, Category> &other) {
 			_ptr = other._ptr;
@@ -59,7 +80,7 @@ namespace ft {
 			++_ptr;
 			return *this;
 		};
-		typename ft::conditional<IsConst, remove_const<T>&, const T&>::type
+		typename ft::conditional<IsConst, T&, const T&>::type
 		operator*() const { return *_ptr; };
 		T* base() const { return _ptr; };
 		virtual ~Iterator() {};
